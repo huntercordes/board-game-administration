86c0cd540b6c1c9d11d52ac9f58ea6a7f0240be8
huntercordes <huntercordes@gmail.com>
2025-03-14 02:31:07 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/board-game-administration/', // Replace 'your-repo-name' with the actual name of your GitHub repository
 })
